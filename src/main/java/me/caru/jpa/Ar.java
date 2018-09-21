4937ae81fca61db0680a76327e347d67221141d0
조경대 <kyungdae.cho@navercorp.com>
2018-09-21 09:15:14 +0900

    1231

---
+++ src/main/java/me/caru/jpa/Ar.java
@@ -0,0 +1,28 @@
+package me.caru.jpa;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Ar
+ *
+ * @author kyungdae.cho
+ * @version 1.0.0
+ * @since 2018. 09. 20.
+ */
+
+@Component
+@Slf4j
+public class Ar implements ApplicationRunner {
+	@Value("${url.a}")
+	private String a;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("a == {}", a);
+	}
+}
