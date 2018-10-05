eaf159f4bbbcb9d6268b6ea38ff9853a163b814e
kyungdae.cho <kyungdae.cho@navercorp.com>
2018-10-05 16:18:01 +0900

    123

---
+++ src/main/java/me/caru/jpa/core/member/MemberRepository.java
@@ -0,0 +1,14 @@
+package me.caru.jpa.core.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+/**
+ * AddressRepository
+ *
+ * @author kyungdae.cho
+ * @version 1.0.0
+ * @since 2018. 10. 05.
+ */
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+}
--- src/main/java/me/caru/jpa/runner/Js.java
+++ src/main/java/me/caru/jpa/runner/Js.java
@@ -9,10 +9,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.caru.jpa.core.address.Address;
 import me.caru.jpa.core.member.Member;
+import me.caru.jpa.core.member.QMember;
 import me.caru.jpa.core.oderitem.OrderItem;
 import me.caru.jpa.core.order.Order;
 import me.caru.jpa.core.order.delivery.Delivery;
@@ -83,8 +86,18 @@ public class Js implements ApplicationRunner {
 			log.info("orderItems = {}", row[1]);
 			log.info("delivery = {}", row[2]);
 		});
-
 		log.info("end");
 
+		JPAQuery<?> jpaQuery = new JPAQuery<Void>(entityManager);
+		QMember qMember = new QMember("m");
+
+		List<MM> members =
+			jpaQuery.select(Projections.bean(MM.class, qMember.id.as("i"), qMember.name.as("n")))
+				.from(qMember)
+				.where(qMember.name.eq("caru1"), qMember.name.eq("caru2"))
+				.orderBy(qMember.name.desc())
+				.offset(1)
+				.limit(1)
+				.fetch();
 	}
 }
